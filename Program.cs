24e0700c67771fd609718f93b60f6094113e7bb2
HassanSena <hassanssena@gmail.com>
2019-06-05 12:43:32 -0300

    05.06

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArquivoTextoFormatado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string Nome;
+            int SobraNome;
+            string DataNascimento;
+            float Salario;
+            int i;
+
+            do
+            {
+                Console.WriteLine("Digite o seu Nome:");
+                Nome = Console.ReadLine();
+
+            } while (Nome.Length > 18);
+
+            SobraNome = 18 - Nome.Length;
+
+            for (i = 0; i < SobraNome; i++)
+            {
+
+                Nome = Nome + " ";
+            }
+
+            Console.WriteLine(Nome.Length);
+
+            Console.ReadKey();
+        }
+    }
+}
